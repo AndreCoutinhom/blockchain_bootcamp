98088abb798455c1481fcc1a50a5b1bf6ddbb298
André Coutinho de Macedo Silva <91290799+AndreCoutinhom@users.noreply.github.com>
2024-02-28 00:48:34 -0300

    Update main.py

--- 1 - Fundamentos/Aula 1/Blockchain 101/main.py
+++ 1 - Fundamentos/Aula 1/Blockchain 101/main.py
@@ -1,3 +1,5 @@
+# O código acima codifica a palavra "nearx" em uma sequência pseudoaleatória. A sequência continuará sendo a mesma não importa quantas vezes o código seja acionado. A menos que a palavra tenha seu valor de caracteres alterados, o código gerado continuará sendo o mesmo.
+
 import hashlib
 
 data = "nearx"
@@ -8,5 +10,4 @@ output_decoded = output.hexdigest()
 
 print(output_decoded)
 
-# O código acima codifica a palavra "nearx" em uma sequência pseudoaleatória. A sequência continuará sendo a mesma não importa quantas vezes o código seja acionado. A menos que a palavra tenha seu valor de caracteres alterados, o código gerado continuará sendo o mesmo.
 # O tamanho da string também não altera o número de caracteres da sequência codificada, ou seja, o resultado do hash é fixo em tamanho de saída.
