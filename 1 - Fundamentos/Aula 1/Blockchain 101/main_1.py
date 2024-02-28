7576d5f993f2981ca031b914000c99171c8ce25d
André Coutinho de Macedo Silva <91290799+AndreCoutinhom@users.noreply.github.com>
2024-02-28 00:47:50 -0300

    Create main_1.py

---
+++ 1 - Fundamentos/Aula 1/Blockchain 101/main_1.py
@@ -0,0 +1,8 @@
+import keccaky
+
+data = "nearx"
+output_decoded = keccaky.hash_it(data)
+
+print(output_decoded)
+
+# Usando bibliotecas diferentes, percebemos que cada função hash produz um resultado diferente.
