c2a2080cce1e5bef05e86542d4536a835f3e2055
André Coutinho de Macedo Silva <91290799+AndreCoutinhom@users.noreply.github.com>
2024-03-06 02:15:58 -0300

    Create retrieve_supply.sh

---
+++ 1 - Fundamentos/Aula 5/Comandos no Terminal/retrieve_supply.sh
@@ -0,0 +1,2 @@
+$ cast call 0x6b175474e89094c44da98b954eedeac495271d0f "totalSupply()(uint256)" --rpc-url https://eth-mainnet.alchemyapi.io/v2/Lc7oIGYeL_QvInzI0Wiu_pOZZDEKBrdf
+3687858787626171424333349502 [3.687e27]
